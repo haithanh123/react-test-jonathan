9920c2c7d1a65cce81f720723363af464f9a9e32
Thanh Truong <thanh.truonghai@ascendcorp.com>
2021-02-19 22:30:58 +0700

    update project

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -1,3 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 export default function Notfound() {
-  return <div>Opps! Page not found</div>;
+  return (
+    <Wrapper>
+      <h2>Opps! Page not found</h2>
+      <span>
+        Comeback to
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </span>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    margin-left: 10px;
+  }
+`;
